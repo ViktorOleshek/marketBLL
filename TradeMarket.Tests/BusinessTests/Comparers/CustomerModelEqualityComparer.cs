d354ffd4cd8dd219772ec59b2e6f93ef454c41e4
Yurii Antonov <yurii_antonov@epam.com>
2024-04-09 18:01:38 +0300

    Initial commit

---
+++ TradeMarket.Tests/BusinessTests/Comparers/CustomerModelEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using Data.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Business.Models;
+using System;
+
+namespace TradeMarket.Tests
+{
+    internal sealed class CustomerModelEqualityComparer : IEqualityComparer<CustomerModel>
+    {
+        public bool Equals([AllowNull] CustomerModel x, [AllowNull] CustomerModel y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id &&
+                x.BirthDate == y.BirthDate &&
+                x.DiscountValue == y.DiscountValue &&
+                string.Equals(x.Name, y.Name, StringComparison.Ordinal) &&
+                string.Equals(x.Surname, y.Surname, StringComparison.Ordinal);
+        }
+
+        public int GetHashCode([DisallowNull] CustomerModel obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ TradeMarket.Tests/BusinessTests/Comparers/ProductModelEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using Data.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Business.Models;
+using System;
+
+namespace TradeMarket.Tests
+{
+    internal sealed class ProductModelEqualityComparer : IEqualityComparer<ProductModel>
+    {
+        public bool Equals([AllowNull] ProductModel x, [AllowNull] ProductModel y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id &&
+                x.ProductCategoryId == y.ProductCategoryId &&
+                string.Equals(x.CategoryName, y.CategoryName, StringComparison.Ordinal) &&
+                string.Equals(x.ProductName, y.ProductName, StringComparison.Ordinal) &&
+                x.Price == y.Price;
+        }
+
+        public int GetHashCode([DisallowNull] ProductModel obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}

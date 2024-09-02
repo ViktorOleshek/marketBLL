a71e4e802919646497bbee81d4906bd4c9de1bb2
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-09-02 04:45:21 +0300

    Implement automapper

--- Business/AutomapperProfile.cs
+++ Business/AutomapperProfile.cs
@@ -1,7 +1,7 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using AutoMapper;
 using Business.Models;
 using Data.Entities;
-using System.Linq;
 
 namespace Business
 {
@@ -9,18 +9,29 @@ namespace Business
     {
         public AutomapperProfile()
         {
-            CreateMap<Receipt, ReceiptModel>()
+            this.CreateMap<Receipt, ReceiptModel>()
                 .ForMember(rm => rm.ReceiptDetailsIds, r => r.MapFrom(x => x.ReceiptDetails.Select(rd => rd.Id)))
                 .ReverseMap();
 
-            //TODO: Create mapping for Product and ProductModel
+            this.CreateMap<Product, ProductModel>()
+                .ForMember(pm => pm.ProductCategoryId, p => p.MapFrom(x => x.ProductCategoryId))
+                .ForMember(pm => pm.CategoryName, p => p.MapFrom(x => x.Category.CategoryName))
+                .ForMember(pm => pm.ReceiptDetailIds, p => p.MapFrom(x => x.ReceiptDetails.Select(rd => rd.Id)))
+                .ReverseMap();
 
-            //TODO: Create mapping for ReceiptDetail and ReceiptDetailModel
+            this.CreateMap<ReceiptDetail, ReceiptDetailModel>()
+                .ReverseMap();
 
-            //TODO: Create mapping that combines Customer and Person into CustomerModel
+            this.CreateMap<Customer, CustomerModel>()
+                .ForMember(cm => cm.ReceiptsIds, c => c.MapFrom(x => x.Receipts.Select(r => r.Id)))
+                .ForMember(cm => cm.Name, c => c.MapFrom(x => x.Person.Name))
+                .ForMember(cm => cm.Surname, c => c.MapFrom(x => x.Person.Surname))
+                .ForMember(cm => cm.BirthDate, c => c.MapFrom(x => x.Person.BirthDate))
+                .ReverseMap();
 
-            //TODO: Create mapping for ProductCategory and ProductCategoryModel
-
+            this.CreateMap<ProductCategory, ProductCategoryModel>()
+                .ForMember(pcm => pcm.ProductIds, pc => pc.MapFrom(x => x.Products.Select(p => p.Id)))
+                .ReverseMap();
         }
     }
 }
